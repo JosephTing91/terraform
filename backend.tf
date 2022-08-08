771bb23d1cf4adb3eccef9ddbc1e6bc993835581
JosephTing91 <43622166+JosephTing91@users.noreply.github.com>
2022-08-08 17:11:22 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
     backend "s3" {
      bucket = "terraform-statefiles-ahjoe"
      key    = "statefileTest.tfstate"
-     region = "us-west-2
+     region = "us-west-2"
      profile = "jespo"
      dynamodb_table= "terraform-project"
     }
